faa380d0db38225f1c017b206a617e6502d03c91
Ashutosh-3456 <ac4817770@gmail.com>
2026-04-26 22:29:20 +0530

    Add files via upload

---
+++ altsrc48.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+x=np.linspace(-10,10,100)
+y=x**2+2*x
+z=np.sin(y)
+plt.plot(x,y,label="y=x**2+2*x")
+plt.plot(x,z,label="z=sin(y)")
+plt.xlabel("x")
+plt.ylabel("Values")
+plt.title("Comparison table")
+plt.legend()
+plt.show()
